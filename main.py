84d676205ff954e8746d6e56a4d08180adcc17a4
Karan PRUTHI <karan.pruthi@amadeus.com>
2022-07-11 02:57:21 +0530

    establish CI CD pipeline with docker and circle ci

---
+++ main.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from wsgiref import simple_server
+from flask import Flask, request, render_template
+from flask import Response, session, jsonify
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    return "Starting Machine Learning Project"
+
+
+port = int(os.getenv("PORT", 5000))
+
+
+if __name__ == "__main__":
+    host = '0.0.0.0'
+    # port = 5000
+    httpd = simple_server.make_server(host, port, app)
+    # print("Serving on %s %d" % (host, port))
+    httpd.serve_forever()
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="travel-prediction",
+    version="0.0.3",
+    description="ML project",
+    author="Karan Pruthi",
+    packages=find_packages(),
+    license="MIT"
+)
